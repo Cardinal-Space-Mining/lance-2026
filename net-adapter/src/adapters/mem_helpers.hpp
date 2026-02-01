297a17d7f32bf2acfb8e818434fb00d33b5b9c2e
Connor Tynan <connortynan16@gmail.com>
2026-01-31 22:14:51 -0600

    added "many" variants for all mem_helper functions

--- net-adapter/src/adapters/mem_helpers.hpp
+++ net-adapter/src/adapters/mem_helpers.hpp
@@ -122,27 +122,24 @@ inline constexpr std::enable_if_t<std::is_enum_v<T>, T> byteSwap(T v)
 template<typename T>
 inline constexpr std::enable_if_t<std::is_floating_point_v<T>, T> byteSwap(T v)
 {
+    // reinterpret float bits as int, swap bytes, reinterpret back to float
     if constexpr (sizeof(T) == 4)
     {
-        uint32_t tmp;
-        std::memcpy(&tmp, &v, sizeof(T));
-        tmp = byteSwap(tmp);
-        std::memcpy(&v, &tmp, sizeof(T));
-        return v;
+        uint32_t u = std::bit_cast<uint32_t>(v);
+        u = byteSwap(u);
+        return std::bit_cast<T>(u);
     }
     else if constexpr (sizeof(T) == 8)
     {
-        uint64_t tmp;
-        std::memcpy(&tmp, &v, sizeof(T));
-        tmp = byteSwap(tmp);
-        std::memcpy(&v, &tmp, sizeof(T));
-        return v;
+        uint64_t u = std::bit_cast<uint64_t>(v);
+        u = byteSwap(u);
+        return std::bit_cast<T>(u);
     }
     else
     {
         static_assert(
             sizeof(T) == 4 || sizeof(T) == 8,
-            "Unsupported floating point size for byteSwap");
+            "Unsupported floating-point size");
     }
 }
 
@@ -185,6 +182,37 @@ inline void writeAndIncrement(uint8_t*& ptr, const T& value)
     ptr += sizeof(T);
 }
 
+template<typename T>
+inline void writeMany(uint8_t* ptr, const std::vector<T>& value)
+{
+    static_assert(
+        HOST_IS_LITTLE_ENDIAN || is_byteswapable_v<T>,
+        "write<T>: T must be a byte-swapable type: integral, floating-point, "
+        "enum, or a user-defined type specializing util::is_byteswapable<T>");
+
+    if constexpr (HOST_IS_LITTLE_ENDIAN)
+    {
+        std::memcpy(ptr, value.data(), value.size() * sizeof(T));
+    }
+    else
+    {
+        uint8_t* tmp_ptr = ptr;
+        for (const T& val : value)
+        {
+            T tmp = byteSwap(val);
+            std::memcpy(tmp_ptr, &tmp, sizeof(T));
+            tmp_ptr += sizeof(T);
+        }
+    }
+}
+template<typename T>
+inline void writeManyAndIncrement(uint8_t*& ptr, const std::vector<T>& value)
+{
+    writeMany(ptr, value);
+
+    ptr += value.size() * sizeof(T);
+}
+
 template<typename StorageT, typename T>
 inline void writeAs(uint8_t* ptr, const T& value)
 {
@@ -210,6 +238,38 @@ inline void writeAsAndIncrement(uint8_t*& ptr, const T& value)
     writeAs<StorageT>(ptr, value);
 
     ptr += sizeof(StorageT);
+}
+
+template<typename StorageT, typename T>
+inline void writeManyAs(uint8_t* ptr, const std::vector<T>& value)
+{
+    static_assert(
+        HOST_IS_LITTLE_ENDIAN || is_byteswapable_v<StorageT>,
+        "writeAs<StorageT>: StorageT must be a byteswapable type: integral, "
+        "floating-point, enum, or a user-defined type specializing "
+        "util::is_byteswapable<StorageT>");
+
+    if constexpr (HOST_IS_LITTLE_ENDIAN && std::is_same_v<T, StorageT>)
+    {
+        std::memcpy(ptr, value.data(), value.size() * sizeof(T));
+    }
+    else
+    {
+        uint8_t* tmp_ptr = ptr;
+        for (const T& val : value)
+        {
+            StorageT tmp = asLittleEndian(static_cast<StorageT>(val));
+            std::memcpy(tmp_ptr, &tmp, sizeof(StorageT));
+            tmp_ptr += sizeof(StorageT);
+        }
+    }
+}
+template<typename StorageT, typename T>
+inline void writeManyAsAndIncrement(uint8_t*& ptr, const std::vector<T>& value)
+{
+    writeManyAs<StorageT>(ptr, value);
+
+    ptr += value.size() * sizeof(StorageT);
 }
 
 template<typename T>
@@ -239,6 +299,41 @@ inline void readAndIncrement(const uint8_t*& ptr, T& out)
     ptr += sizeof(T);
 }
 
+template<typename T>
+inline void readMany(const uint8_t* ptr, std::vector<T>& out, size_t n)
+{
+    static_assert(
+        HOST_IS_LITTLE_ENDIAN || is_byteswapable_v<T>,
+        "read<T>: T must be a byteswapable type: integral, floating-point, "
+        "enum, or a user-defined type specializing util::is_byteswapable<T>");
+
+    if constexpr (HOST_IS_LITTLE_ENDIAN)
+    {
+        out.resize(n);
+        std::memcpy(out.data(), ptr, n * sizeof(T));
+    }
+    else
+    {
+        out.resize(n);
+        const uint8_t* tmp_ptr = ptr;
+        for (size_t i = 0; i < n; ++i)
+        {
+            T tmp;
+            std::memcpy(&tmp, tmp_ptr, sizeof(T));
+            out[i] = byteSwap(tmp);
+            tmp_ptr += sizeof(T);
+        }
+    }
+}
+template<typename T>
+inline void
+    readManyAndIncrement(const uint8_t*& ptr, std::vector<T>& out, size_t n)
+{
+    readMany(ptr, out, n);
+
+    ptr += n * sizeof(T);
+}
+
 template<typename StorageT, typename T>
 inline void readAs(const uint8_t* ptr, T& out)
 {
@@ -266,6 +361,43 @@ inline void readAsAndIncrement(const uint8_t*& ptr, T& out)
     readAs<StorageT>(ptr, out);
 
     ptr += sizeof(StorageT);
+}
+
+template<typename StorageT, typename T>
+inline void readManyAs(const uint8_t* ptr, std::vector<T>& out, size_t n)
+{
+    static_assert(
+        HOST_IS_LITTLE_ENDIAN || util::is_byteswapable_v<StorageT>,
+        "readAs<StorageT>: T must be a byteswapable type: integral, "
+        "floating-point, enum, or a user-defined type specializing "
+        "util::is_byteswapable<StorageT>");
+
+
+    if constexpr (HOST_IS_LITTLE_ENDIAN && std::is_same_v<T, StorageT>)
+    {
+        out.resize(n);
+        std::memcpy(out.data(), ptr, n * sizeof(StorageT));
+    }
+    else
+    {
+        out.resize(n);
+        const uint8_t* tmp_ptr = ptr;
+        for (size_t i = 0; i < n; ++i)
+        {
+            StorageT tmp{};
+            std::memcpy(&tmp, tmp_ptr, sizeof(StorageT));
+            out[i] = static_cast<T>(asLittleEndian(tmp));
+            tmp_ptr += sizeof(StorageT);
+        }
+    }
+}
+template<typename StorageT, typename T>
+inline void
+    readManyAsAndIncrement(const uint8_t*& ptr, std::vector<T>& out, size_t n)
+{
+    readManyAs<StorageT>(ptr, out, n);
+
+    ptr += n * sizeof(StorageT);
 }
 
 }  // namespace util
