1623b8de5a50d7838920f16bd12eb2c224de5ddd
Cale Augustine <caleaug@gmail.com>
2026-05-02 17:46:40 -0500

    Fixed the arduino error and cleaned the python code

--- lance/scripts/hopper_fullness.py
+++ lance/scripts/hopper_fullness.py
@@ -11,36 +11,13 @@ import serial, sys, glob
 
 # this is just code I took that find what port the arduino is connected
 def serial_ports() -> list[str]:
-    if sys.platform.startswith("win"):
-        ports = ["COM%s" % (i + 1) for i in range(256)]
-    elif sys.platform.startswith("linux") or sys.platform.startswith("cygwin"):
-        # ports = glob.glob("/dev/tty[A-Za-z]*")
-        ports = glob.glob("/dev/serial/by-id/*Arduino*")
-    elif sys.platform.startswith("darwin"):
-        ports = glob.glob("/dev/tty.*")
-    else:
+    try: 
+        ports = glob.glob("/dev/serial/by-id/*1a86*")
+    except:
+        ports = []
         raise EnvironmentError("Unsupported platform")
 
-    result = []
-    for port in ports:
-        try:
-            s = serial.Serial(port)
-            s.close()
-            result.append(port)
-        except (OSError, serial.SerialException):
-            pass
-    return result
-
-
-# #Just gets the data from serial with checking for erros and finding the port if it changes or arduino get unpluged
-# def DataOut():
-#     try:
-#         data = globals()["ser"].readline().decode().strip()
-#         return data
-#     except:
-#         try: globals()["ser"] = serial.Serial(serial_ports()[0],9600)
-#         except: pass
-
+    return ports
 
 # this is just a publish I stole the code from most of it I left the same but I added code to timer_callback
 class MinimalPublisher(Node):
@@ -61,7 +38,12 @@ class MinimalPublisher(Node):
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
         if serial_port is None:
-            self.serial_port = serial.Serial(serial_ports()[0], 9600)
+            while True:
+                try:
+                    self.serial_port = serial.Serial(serial_ports()[0], 9600)
+                    break
+                except (serial.SerialException, IndexError):
+                    continue
         elif isinstance(serial_port, str):
             self.serial_port = serial.Serial(serial_port, 9600)
         else:
