1fa61d5d964a573d7d5d3947ee04794c62c8e970
caper1399 <manny@reillyweb.com>
2026-05-16 22:15:04 -0400

    it worked at home.

--- lance/src/robot/control/shared/mining_controller.cpp
+++ lance/src/robot/control/shared/mining_controller.cpp
@@ -299,12 +299,12 @@ void MiningController::iterate(
                 if (hopper_act_val >
                     this->params.hopper_actuator_traversal_target_val)
                 {
-                    commands.setHopperActVelocity(
+                    commands.setHopperActSpeed(
                         -this->params.hopper_actuator_max_speed);
                 }
                 else
                 {
-                    commands.setHopperActVelocity(
+                    commands.setHopperActSpeed(
                         -this->params.hopper_actuator_plunge_speed);
                 }
                 break;
@@ -419,12 +419,12 @@ void MiningController::iterate(
                 }
                 else if (hopper_val < hopper_act_target)
                 {
-                    commands.setHopperActVelocity(
+                    commands.setHopperActSpeed(
                         this->params.hopper_actuator_plunge_speed);
                 }
                 else if (hopper_val > hopper_act_target)
                 {
-                    commands.setHopperActVelocity(
+                    commands.setHopperActSpeed(
                         -this->params.hopper_actuator_plunge_speed);
                 }
             }
@@ -438,7 +438,7 @@ void MiningController::iterate(
             {
                 commands.setTrencherVelocity(
                     this->params.trencher_mining_velocity_rps);
-                commands.setHopperActVelocity(
+                commands.setHopperActSpeed(
                     this->params.hopper_actuator_extract_speed);
                 break;
             }
